bead9c0a8d5674555acdffa687859ea4821bc86a
Swayanshu Satyapragyan Sahoo <125992313+Swayanshu004@users.noreply.github.com>
2024-10-25 19:25:59 +0530

    prepare for deploy

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
   }
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://amitdevdiary.vercel.app/'); 
+  res.header('Access-Control-Allow-Origin', 'https://amitdevdiary.vercel.app'); 
   res.header('Access-Control-Allow-Credentials', 'true'); 
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
