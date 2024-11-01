768615c1d6872f47454d81f3742d37693472e93d
Swayanshu Satyapragyan Sahoo <125992313+Swayanshu004@users.noreply.github.com>
2024-11-01 19:28:39 +0530

    prepare for deploy

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import cors from 'cors'
 const app = express();
 
 const corsOptions = {
-    origin: 'https://amitdevdiary.vercel.app',
+    origin: 'https://devdiary.codingaashram.com',
     methods: "GET, POST, PATCH, DELETE",
     credential: true
   }
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://amitdevdiary.vercel.app'); 
+  res.header('Access-Control-Allow-Origin', 'https://devdiary.codingaashram.com'); 
   res.header('Access-Control-Allow-Credentials', 'true'); 
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
